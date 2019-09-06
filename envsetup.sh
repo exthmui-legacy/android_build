5cbfa4ffc43d01e08bae77bc2f9d11c7946052e6
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-09-06 18:31:15 +0200

    Automatically set CCACHE_EXEC to the system's ccache
    * Check for ccache tool and set its path in CCACHE_EXEC if it hasn't been set already
    
    * Enable ccache and compression by default
    
    Change-Id: Ife9d0a723cb90b89392ee288ebe6e7e9f2be7eef
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1641,6 +1641,24 @@ validate_current_shell
 source_vendorsetup
 addcompletions
 
+function repopick() {
+    T=$(gettop)
+    $T/vendor/dot/build/tools/repopick.py $@
+}
+
+# check and set ccache path on envsetup
+if [ -z ${CCACHE_EXEC} ]; then
+    ccache_path=$(which ccache)
+    if [ ! -z "$ccache_path" ]; then
+	export USE_CCACHE=1
+	export CCACHE_COMPRESS=1
+        export CCACHE_EXEC="$ccache_path"
+	echo -e "\emccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \emhas been set to : \e[4m$ccache_path\e[0m"
+    else
+        echo -e "\e[31m\emccache not found/installed!\e[0m"
+    fi
+fi
+
 export ANDROID_BUILD_TOP=$(gettop)
 
 . $ANDROID_BUILD_TOP/vendor/exthm/build/envsetup.sh
